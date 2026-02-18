976e18c2eba1d8fa30d22e7c9ed81b19ae05586b
Антон Довиденков <ALDovidenkov@greenatom.ru>
2026-02-18 17:23:15 +0300

    Update video_uploader.py

--- logistic/service/video_uploader.py
+++ logistic/service/video_uploader.py
@@ -3,14 +3,17 @@ from pytubefix.exceptions import MaxRetriesExceeded
 
 class VideoUploader:
 
-    def __init__(self, url):
+    def __init__(self, url, low_resolution=False):
         self.url = url
         self.video_file = None
+        self.low_resolution = low_resolution
 
     def upload(self):
         try:
             yt = YouTube(self.url)
-            self.video_file = yt.streams.get_highest_resolution().download()
+            streams = yt.streams
+            stream = streams.get_lowest_resolution() if self.low_resolution else streams.get_highest_resolution()
+            self.video_file = stream.download()
         except MaxRetriesExceeded:
             self.video_file = None
         return self.video_file
