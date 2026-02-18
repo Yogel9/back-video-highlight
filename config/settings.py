671fdc9d29ae54e4bccc47affee9cddae301945f
Антон Довиденков <ALDovidenkov@greenatom.ru>
2026-02-18 15:44:47 +0300

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-t^xux8_85)cj%qf@l41i95%1#tw1rsd&@w)!vib&e!+v3vw_*!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
