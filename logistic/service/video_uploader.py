503d947aa22a5f3310f66fd1a25c328a4cb8ffb0
Антон Довиденков <ALDovidenkov@greenatom.ru>
2026-02-18 16:47:14 +0300

    3

--- logistic/service/video_uploader.py
+++ logistic/service/video_uploader.py
@@ -4,8 +4,9 @@ class VideoUploader:
 
     def __init__(self, url):
         self.url = url
+        self.video_file = None
 
     def upload(self):
         yt = YouTube(self.url)
         self.video_file = yt.streams.get_highest_resolution().download()
-        return self.file
+        return self.video_file
