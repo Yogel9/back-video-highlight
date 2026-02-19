9d1381f8db104c94b16b45eaffe55c31989b76e2
Антон Довиденков <ALDovidenkov@greenatom.ru>
2026-02-19 12:43:52 +0300

    Update utils.py

--- logistic/utils.py
+++ logistic/utils.py
@@ -7,11 +7,11 @@ def get_public_media_url(url: str | None) -> str | None:
         return None
 
     base_url = getattr(settings, "MEDIA_PUBLIC_BASE_URL", None)
-
+    full_url = base_url + "/video"
     if not base_url:
         return url
-
-    if "minio" in url:
-        return url.replace("minio", base_url)
+    for pref in ["https://minio:9000", "minio:9000", "http://minio:9000"]
+        if pref in url:
+            return url.replace(pref, full_url)
 
     return url
